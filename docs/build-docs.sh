108ed2ad7e1b0fc670794519292bacce8c260182
JUI-TE LIN <fredlin@JUI-TEs-MacBook-Pro-1080.local>
2021-11-10 20:16:42 -0800

    api

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -12,7 +12,7 @@ pip3 install sphinx_rtd_theme
 pip3 install code_include.extension
 pip3 install sphinx-code-include
 pip3 install scipy
-pip3 install openmdao==2.4.0
+pip3 install openmdao
 
 
 # build docs
--- setup.py
+++ setup.py
@@ -10,13 +10,11 @@ setup(
         'numpy',
         'dash==1.2.0',
         'dash-daq==0.1.0',
-        'openmdao==2.4.0',
         'pint',
         'guppy3',
         'sphinx-rtd-theme',
         'sphinx-code-include',
         'jupyter-sphinx',
         'numpydoc',
-        'scipy'
     ],
 )
